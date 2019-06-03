614b6dd35ea95d9bd184efd6112ce454483ae63b
AlexRaschid <alexraschid1337@gmail.com>
2019-06-03 01:53:22 -0400

    Able to render google map but cant seem to update the state with the device's current 'geo-position'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import GoogleMapReact from 'google-map-react';
 import './App.css';
+import {MyMap} from './components/MyMap.js';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      currentPos: {
+        lat: 12,
+        lng: 12,
+      }
+    }
+  }
+
+  componentDidMount(){
+    this.findCoordinates();
+  }
+
+  findCoordinates = () => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const location = JSON.stringify(position);
+
+        this.setState({ currentPos:{
+          lat: location.coords.latitude,
+          lng: location.coords.longitude,
+        } });
+      },
+      error => console.log(error.message),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+    console.log(this.state);
+  };
+
+
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <MyMap></MyMap>
+        
+        <div style={{ height: '100vh', width: '100%' }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: 'AIzaSyCZ6cSChM_EVpFY_iYNeo_plSDBogPYoPY' }}
+            defaultCenter={{lat: this.state.currentPos.lat, 
+                            lng: this.state.currentPos.lng}}
+            defaultZoom={10}
+          ></GoogleMapReact>
+        </div>
       </div>
     );
   }
